e4320e03b52237b0857dab58982b9be216eb9ac8
Jyarn <jasonyee2003@gmail.com>
2021-04-17 17:25:56 -0400

    new file:   config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+wallSize = 1
+pathSize = 25
+elmSz = wallSize + pathSize
+
+winRes = [720, 720]
+size = [ int(winRes[0] / elmSz) * elmSz+wallSize, int(winRes[1] / elmSz) * elmSz+wallSize] # window size
+
+# 0 = wall colour
+# 1 = path colour
+colours = [ [128, 0, 0], [0, 0, 0] ]
+screen = None
